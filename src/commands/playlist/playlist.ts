b1f1992164d9a856d513318fabd4c9adb2413344
Ari Kishon <ariki@wix.com>
2023-04-25 14:06:28 +0300

    fix: better error handling for bad responses

--- src/commands/playlist/playlist.ts
+++ src/commands/playlist/playlist.ts
@@ -1,30 +1,36 @@
-import { getOpenAiApi } from '../../helpers';
+import { getOpenAiApi, printLog } from '../../helpers';
 import { BaseCommandParams } from '../types';
 import { generateDescription, generatePlaylist, generateTitle } from './helpers';
 
 export const playlist = async (prompt: string, { requestParams }: BaseCommandParams) => {
     const openai = getOpenAiApi();
+
     const playlist = await generatePlaylist({ prompt, baseCommandParams: { requestParams }, openai });
-    const title = await generateTitle({ prompt: playlist ?? '', baseCommandParams: { requestParams }, openai });
+    const lines = playlist.split('\n').filter((line) => line);
+    const unexpectedResults = lines.filter((line) => line.length > 80);
+    const expectedResult = lines.filter((line) => line.length < 80).join('\n');
+    printLog(
+        unexpectedResults.length > 0,
+        `Unexpected results from GPT:\n${unexpectedResults.join('\n')}`,
+        'BgRed'
+    );
+
+    const title = await generateTitle({ prompt: expectedResult ?? '', baseCommandParams: { requestParams }, openai });
     const description = await generateDescription({
-        prompt: playlist ?? '',
+        prompt: expectedResult ?? '',
         baseCommandParams: { requestParams },
         openai,
     });
 
     return {
-        songs: playlist
-            .split('\n')
-            .filter((line) => line)
-            .filter((line) => line.length < 80) // Try to filter out none-song results
-            .map((line) => {
-                const split = line.split('-');
-                return {
-                    song: split[1]?.trim() ?? '',
-                    artist: split[0].trim(),
-                };
-            }),
-        title: title,
-        description: description,
+        songs: expectedResult.split('\n').map((line) => {
+            const split = line.split('-');
+            return {
+                song: split[1]?.trim() ?? '',
+                artist: split[0].trim(),
+            };
+        }),
+        title,
+        description,
     };
 };
--- src/helpers/printing/print-log.ts
+++ src/helpers/printing/print-log.ts
@@ -1,5 +1,5 @@
-import { Color, colorPrint } from './color-print';
+import { Color, getColorText } from './color-print';
 
 export const printLog = (on: boolean, text: string, color: Color = 'FgYellow') => {
-    if (on) colorPrint(color, `[!] ${text.replace(new RegExp('\\n', 'g'), '\n    ')}\n`);
+    if (on) console.log(`${getColorText(color, `[!] ${text.replace(new RegExp('\\n', 'g'), '\n    ')}`)}\n`);
 };
